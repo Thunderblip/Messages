5128bd8b4dc4364988534898793df2d4ca8a293b
Frederick Hickling <hicf1_16@uni.worc.ac.uk>
2017-12-04 14:06:49 +0000

    Added folder

---
+++ js/index.js
@@ -0,0 +1,91 @@
+var notification_count=0;
+
+$(document).ready()
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.notification);
+}
+
+$(document).on('pageinit', function() {
+
+	$('#messageButton').on('click', function() {
+		createMessage();
+	});
+    
+    $('#messageButton2').on('click', function() {
+		createMessage2();
+	});
+	
+	$('#dialogButton').on('click', function() {
+		createDialog();
+	});
+
+
+	$('#notificationButton').on('click', function() {
+		createNotification();
+	});
+
+
+});
+
+
+
+function createMessage(){		
+	//phoneGap and jQueryMobile do not support toast messages directly
+    //so we can add this using toast.js
+    new Toast({content: 'Moshi moshi, genki desuka?', duration: 5000}); 
+}
+
+function createMessage2(){		
+	//phoneGap and jQueryMobile do not support toast messages directly
+    //so we can add this using toast.js
+    new Toast({content: 'NANI?', duration: 1000}); 
+}
+        	
+
+function createDialog() {
+
+	//phonegap supports native dialog boxes.
+	//here's a simple example
+      
+	navigator.notification.confirm(
+    	'What do you think of this dialog?',  // message
+        dialogDismissed,         // callback
+        'An example dialog!',            // title
+        ['Awesome!', 'Sucks']                  // buttons
+    );
+
+}
+        	
+        	
+        	
+function dialogDismissed(buttonIndex) {
+	
+	if(buttonIndex==1) new Toast({content: "You're easily pleased", duration: 3000});
+   	else if(buttonIndex==2) new Toast({content: 'It is rather boring.', duration: 3000});
+
+}
+
+   
+   
+function createNotification() {
+        		
+	//
+    //generate a time to post notification
+    //
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 1000); //delayed time  - add 1 second
+    			
+    //
+    //setup notification
+    //
+    
+    cordova.plugins.notification.local.schedule({ 
+    	id: 		1,
+        title: 		"Hey you",
+        message: 	"This is an example notification",
+        date: 		notificationTime, 
+        badge: 		notification_count++
+   	});
+    
+}
